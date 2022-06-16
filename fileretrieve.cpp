21a5186f8c600567fa816e9ec90c55f58c46f766
ndk256 <64850798+ndk256@users.noreply.github.com>
2022-06-16 09:43:54 -0400

    Clean up comments

--- fileretrieve.cpp
+++ fileretrieve.cpp
@@ -1,6 +1,6 @@
 #include "header.h"
 #include <iostream>
-#include <fstream>//necc?
+#include <fstream> //neccesary?
 // Stuff for AJAX
 #include "cgicc/Cgicc.h"
 #include "cgicc/HTTPHTMLHeader.h"
@@ -24,41 +24,32 @@ form_iterator filename = cgi.getElement("fname");
 fname=**filename;
 
 cout << "Content-Type: text/plain\n\n";
-//cout<<"ok";//TEST
 
 selection = **type;
 
-//cout<<"received file name " << fname;//debug
-
 if(selection=="g"||selection=="G"){
-  file.open("/usr/local/var/www/txt-bin/"+fname);
+  file.open("/usr/local/var/www/txt-bin/"+fname); //replace this with your file path for text files
   if(!file.fail())
   {
     map.grid_init(file);
     file.close();
-    //cout << "Creating " << map.gx << " by " << map.gy << " map.\n";
   }
   else{
-    //cout << "File load failed.";
     string sample_map = "                                                                                                    ";
     map.grid_init(10,10,sample_map);
     //current default is a blank 10x10 grid
-    //cout << "Creating " << map.gx << " by " << map.gy << " map.\n";
   }
 
   map.print_state();
 }
 else if(selection=="p"||selection=="P"){
-  file.open("/usr/local/var/www/txt-bin/"+fname);
+  file.open("/usr/local/var/www/txt-bin/"+fname); //replace this with your file path for text files
   if(!file.fail())
   {
     map.ppl_init(file);
     file.close();
   }
   else{
-    //cout << "Content-Type: text/plain\n\n";
-    //cout << "Failed to load file, using default population";
-    
     string sample_register = "I 0 0 1 1 none\nS 1 1 1 0 surgical\nI 3 4 -1 0 cloth\nS 4 5 0 1 cloth\nS 7 7 -1 -1 KN95";
     map.ppl_init(sample_register);
   }
