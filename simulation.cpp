82dbfc6631a011c6dc87360209ab56faa04437d6
ndk256 <64850798+ndk256@users.noreply.github.com>
2022-06-16 09:50:21 -0400

    Clean up code

--- simulation.cpp
+++ simulation.cpp
@@ -1,6 +1,6 @@
 #include "header.h"
 #include <iostream>
-#include <fstream>//necc?
+#include <fstream> //neccessary?
 // Stuff for AJAX
 #include "cgicc/Cgicc.h"
 #include "cgicc/HTTPHTMLHeader.h"
@@ -17,9 +17,6 @@ int main()
   Map map;
   ifstream file2;
 
-  //string sample_map = "                                                                                                    ";
-//"XXX X   X      XX X X    XXX X     XX X X    XXX X     XX X X    XXX X      X X X     XX X X       X";
-
   // Create AJAX objects to receive information from web page.
   form_iterator x = cgi.getElement("xdim");
   form_iterator y = cgi.getElement("ydim");
@@ -32,64 +29,36 @@ int main()
   string pname = **pop;
   bool pop_load_file = (**pop_fromfile == "1");
   
-//  sample_map=**grid;
   int gx = stoi(**x); int gy = stoi(**y);
   
-  cout << "Content-Type: text/plain\n\n";
-  //cout << "Received:\n\n";
-  //cout << gname;
-//  cout << "\nwhich is " << gname.length() << " vs " << gx*gy;
-
-  /*file.open("/var/www/html/class/softdev/old/kelleher1/web/capstone/files/"+gname);
-  if(!file.fail())
-  {
-    map.grid_init(file);
-  }
-  else{
-    string sample_map = "                                                                                                    ";
-    map.grid_init(10,10,sample_map);
-    //current default is a blank 10x10 grid
-  }
-  file.close();
-  */
-//  cout << "init with " << gx << " x " << gy << " using: =";//debug
-//  cout << gname << "=";//debug
+  cout << "Content-Type: text/plain\n\n"; //start of response
   map.grid_init(gx,gy,gname);
   
-//  sample_register = **pop;
 if(pop_load_file){
   file2.open("/usr/local/var/www/txt-bin/"+pname);
   if(!file2.fail())
   {
     map.ppl_init(file2);
     file2.close();
-    //cout << "printing people\n";//debug
-    //map.print_ppl();//temp
   }
   else{
-    //cout << "Content-Type: text/plain\n\n";
-    //cout << "Failed to load file, using default population";
+    //failed to load the file
     string sample_register = "I 2 3 1 1 none\nS 3 3 -1 1 surgical\nI 3 4 -1 0 cloth\nS 4 5 0 1 cloth\nS 7 7 -1 -1 KN95";
     map.ppl_init(sample_register);
-    //map.print_ppl();
   }
 }
 else{
-  //cout << "received:"<<pname<<"=";
   map.ppl_init(pname);
 }
   
   int ts=0;
   if(stoi(**t)>0) ts = stoi(**t);
 
-  //cout << "Content-Type: text/plain\n\n";
-  //cout<<"Starting map:\n";
   map.print_state(); //prints starting map
   cout << "=";
   
   for(int t=0; t<ts; t++){
     map.update(t);
-    //map.print_ppl(); cout << "=";//DEBUG 
     map.print_state();
     cout << "=";
   }
